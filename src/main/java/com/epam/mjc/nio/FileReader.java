642c71116a742896000fa1c63099a33ee6d96263
Stanislau Marudau <marudov.stas@gmail.com>
2022-08-29 15:12:51 +0300

    1st try

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
-
+import java.io.*;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String[] nameLine = null;
+        String[] ageLine = null;
+        String[] emailLine = null;
+        String[] phoneLine = null;
+        String name = "";
+        Integer age = 0;
+        String email = "";
+        Long phone = 0L;
+
+        try (FileInputStream input = new FileInputStream(file)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            nameLine = reader.readLine().replace(" ", "").split(":");
+            ageLine = reader.readLine().replace(" ", "").split(":");
+            emailLine = reader.readLine().replace(" ", "").split(":");
+            phoneLine = reader.readLine().replace(" ", "").split(":");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (nameLine != null && ageLine != null && emailLine != null && phoneLine != null) {
+            name = nameLine[1];
+            age = Integer.valueOf(ageLine[1]);
+            email = emailLine[1];
+            phone = Long.valueOf(phoneLine[1]);
+        }
+        return new Profile(name, age, email, phone);
     }
+
 }
